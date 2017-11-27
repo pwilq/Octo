c39bed4719c2a4eb54bef2c4fa36dc8c38e10495
pwilq <pawelwilk79@gmail.com>
2017-11-27 10:21:38 +0100

    Commits Count

--- Octo/Program.cs
+++ Octo/Program.cs
@@ -24,21 +24,38 @@ namespace Octo
         {
 
             var gitclient = new GitHubClient(new Octokit.ProductHeaderValue("pwilq"));
+
+
+
             var user = await gitclient.User.Get("pwilq");
 
-            Console.WriteLine("Username: {0}; PublicRepo {1}; Url: {2}",
-                user.Name,
-                user.PublicRepos,
-                user.Url);
-
-            Console.ReadKey();
+            Console.WriteLine("Username: {0}; PublicRepo {1}; Url: {2}", user.Name, user.PublicRepos, user.Url);
 
             var branch = await gitclient.Repository.Branch.GetAll("pwilq", "Octo");
+            var myrepo = await gitclient.Repository.Get("pwilq", "Octo");
+            var commits = await gitclient.Repository.Commit.GetAll("pwilq", "Octo");
+
+            Console.WriteLine("Branch Count: " + branch.Count);
 
             for (int i = 0; i < branch.Count; i++)
             {
-                Console.WriteLine(branch[i].Name + "; commit: " + branch[i].Commit);
+                Console.WriteLine("Branch: "+ branch[i].Name);
             }
+
+            Console.WriteLine("Commits Count: " + commits.Count);
+
+            foreach (var com in commits)
+            {
+                
+                var commit = await gitclient.Repository.Commit.Get("pwilq", "Octo", com.Sha);
+                var files = commit.Files;
+                
+                foreach (var f in files)
+                {
+                    Console.WriteLine(f.Filename);
+                }
+            }
+
 
             Console.ReadKey();
 
